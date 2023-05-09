a1a013d8d1b6765f4eceb4e010b368b88729842a
Rushikesh Naik <34379281+rushikeshnaik779@users.noreply.github.com>
2023-05-09 16:21:01 +0530

    Add files via upload

---
+++ ChatGPT-TRY-1/Chatgpt-streamlit/chatbot-online/utils.py
@@ -0,0 +1,23 @@
+import openai
+
+def get_initial_message():
+    messages=[
+            {"role": "system", "content": "You are AI code generator python webbased application with streamlit"},
+            {"role": "system", "content": "Ask details about the app that user want, like what code they want, where they want to upload it, how they will plan to run the codes etc"},
+            {"role": "user", "content": "User is trying to get the application from your code."},
+            {"role": "assistant", "content": "Sure, let's start building your app"}
+        ]
+    return messages
+
+def get_chatgpt_response(messages, model="gpt-3.5-turbo"):
+    print("model: ", model)
+    print(messages)
+    response = openai.ChatCompletion.create(
+    model=model,
+    messages=messages
+    )
+    return  response['choices'][0]['message']['content']
+
+def update_chat(messages, role, content):
+    messages.append({"role": role, "content": content})
+    return messages
